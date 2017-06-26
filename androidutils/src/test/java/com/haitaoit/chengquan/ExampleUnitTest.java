d0671a8d824738288687a2db74f5f9ba8a1fcc5c
Administrator <2380253499@qq.com>
2017-06-26 11:38:29 +0800

    测试文件

---
+++ androidutils/src/test/java/com/haitaoit/chengquan/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.haitaoit.chengquan;
+
+import com.github.tools.AES;
+
+import org.junit.Test;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        String asf = AES.decode("7412ECF3CA29F73517D58FCDB867841BD7B8D06D182FD06BA9CCD50A3F1FDE78FB24F5F5123B836793470676B45B64D4");
+        String asf2 = AES.encode("");
+        System.out.println(asf);
+        System.out.println(asf2);
+
+    }
+}
---
+++ app/src/test/java/com/haitaoit/chengquan/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package com.haitaoit.chengquan;
+
+import org.junit.Test;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        System.out.println(11);
+
+    }
+}
