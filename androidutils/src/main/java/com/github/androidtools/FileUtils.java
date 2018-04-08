d2f671e39105085bc4ec9f2faa1e991f4597e0bb
2380253499@qq.com <2380253499@qq.com>
2018-04-08 18:47:27 +0800

    工具类

--- androidutils/src/main/java/com/github/androidtools/FileUtils.java
+++ androidutils/src/main/java/com/github/androidtools/FileUtils.java
@@ -2,6 +2,7 @@ package com.github.androidtools;
 
 import android.content.Context;
 import android.content.res.AssetManager;
+import android.util.Log;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -130,6 +131,7 @@ public class FileUtils {
             e.printStackTrace();
         }
     }
+
     /**
      * 将Assets下的文件复制到指定目录下
      *
@@ -138,6 +140,102 @@ public class FileUtils {
      * @param filePath
      */
     public static void copyFileForAssets(Context context, String fileName, String filePath) {
-        copyFileForAssets(context,fileName,new File(filePath));
+        copyFileForAssets(context, fileName, new File(filePath));
     }
+
+
+    /**
+     * 删除文件，可以是文件或文件夹
+     *
+     * @param file 要删除的文件名
+     * @return 删除成功返回true，否则返回false
+     */
+    public static boolean delete(File file) {
+        String fileName=file.getAbsolutePath();
+        if (!file.exists()) {
+            Log.i("===","删除文件失败:" + fileName + "不存在！");
+            return false;
+        } else {
+            if (file.isFile())
+                return deleteFile(fileName);
+            else
+                return deleteDirectory(fileName);
+        }
+    }
+    public static boolean delete(String fileName) {
+        File file = new File(fileName);
+        return delete(file);
+    }
+
+    /**
+     * 删除单个文件
+     *
+     * @param fileName 要删除的文件的文件名
+     * @return 单个文件删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String fileName) {
+        File file = new File(fileName);
+        // 如果文件路径所对应的文件存在，并且是一个文件，则直接删除
+        if (file.exists() && file.isFile()) {
+            if (file.delete()) {
+                Log.i("===","删除单个文件" + fileName + "成功！");
+                return true;
+            } else {
+                Log.i("===","删除单个文件" + fileName + "失败！");
+                return false;
+            }
+        } else {
+            Log.i("===","删除单个文件失败：" + fileName + "不存在！");
+            return false;
+        }
+    }
+
+    /**
+     * 删除目录及目录下的文件
+     *
+     * @param dir 要删除的目录的文件路径
+     * @return 目录删除成功返回true，否则返回false
+     */
+    public static boolean deleteDirectory(String dir) {
+        // 如果dir不以文件分隔符结尾，自动添加文件分隔符
+        if (!dir.endsWith(File.separator))
+            dir = dir + File.separator;
+        File dirFile = new File(dir);
+        // 如果dir对应的文件不存在，或者不是一个目录，则退出
+        if ((!dirFile.exists()) || (!dirFile.isDirectory())) {
+            Log.i("===","删除目录失败：" + dir + "不存在！");
+            return false;
+        }
+        boolean flag = true;
+        // 删除文件夹中的所有文件包括子目录
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            // 删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if (!flag)
+                    break;
+            }
+            // 删除子目录
+            else if (files[i].isDirectory()) {
+                flag = deleteDirectory(files[i]
+                        .getAbsolutePath());
+                if (!flag)
+                    break;
+            }
+        }
+        if (!flag) {
+            Log.i("===","删除目录失败！");
+            return false;
+        }
+        // 删除当前目录
+        if (dirFile.delete()) {
+            Log.i("===","删除目录" + dir + "成功！");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
