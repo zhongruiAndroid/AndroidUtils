26e6cd39638a8114c2c51f151a3514837c4619a5
Administrator <zr2013123@sina.com>
2019-01-14 14:27:38 +0800

    新增截屏功能

--- androidutils/src/main/java/com/github/androidtools/BitmapUtils.java
+++ androidutils/src/main/java/com/github/androidtools/BitmapUtils.java
@@ -1,18 +1,96 @@
 package com.github.androidtools;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.LightingColorFilter;
 import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.FloatRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.content.res.AppCompatResources;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.io.FileDescriptor;
 import java.io.InputStream;
 
 public class BitmapUtils {
+    public static Bitmap createBitmapFromView(View view, int leftCrop, int topCrop, int rightCrop, int bottomCrop){
+        Bitmap originBitmap = createBitmapFromView(view,1);
+        if (originBitmap == null) {
+            return null;
+        }
+        Bitmap cutBitmap=createBitmapSafely(view.getWidth()-leftCrop-rightCrop,view.getHeight()-topCrop-bottomCrop,Bitmap.Config.ARGB_8888,1);
+        if(cutBitmap!=null){
+            Canvas canvas = new Canvas(cutBitmap);
+            canvas.drawColor(Color.WHITE);
+            Rect originRect=new Rect(leftCrop,topCrop,view.getWidth()-rightCrop,view.getHeight()-bottomCrop);
+            RectF cutRect=new RectF(0,0,view.getWidth()-rightCrop-leftCrop,view.getHeight()-bottomCrop-topCrop);
+            canvas.drawBitmap(originBitmap,originRect,cutRect,null);
+            canvas.setBitmap(null);
+            canvas=null;
+            originBitmap.recycle();
+            return cutBitmap;
+        }
+        return cutBitmap;
+    }
+    public static Bitmap createBitmapFromView(View view){
+        return createBitmapFromView(view,1);
+    }
+    public static Bitmap createBitmapFromView(View view,float scale){
+        if(scale<=0){
+            Log.e("screenshots","bitmap scale size not less than 0");
+        }
+        if(view instanceof ImageView){
+            Drawable drawable = ((ImageView) view).getDrawable();
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            return bitmap;
+        }
+        view.clearFocus();
+        Bitmap bitmap=createBitmapSafely(view.getWidth(),view.getHeight(),Bitmap.Config.ARGB_8888,1);
+        if(bitmap!=null){
+            Canvas canvas=new Canvas(bitmap);
+            canvas.drawColor(Color.WHITE);
+            canvas.scale(scale, scale);
+            view.draw(canvas);
+            canvas.setBitmap(null);
+            canvas=null;
+            return bitmap;
+        }
+        return bitmap;
+    }
+    private static Bitmap createBitmapSafely(int width, int height, @NonNull Bitmap.Config config, int retryCount){
+        try {
+            return Bitmap.createBitmap(width,height,config);
+        }catch (OutOfMemoryError error){
+            error.printStackTrace();
+            if(retryCount>0){
+                System.gc();
+                return createBitmapSafely(width,height,config,retryCount-1);
+            }
+        }
+        return null;
+    }
+
 
     /*******************************************************************************************************/
     /**
@@ -587,4 +665,153 @@ public class BitmapUtils {
         return new int[]{options.outWidth,options.outHeight};
     }
 
+    /**
+     * 创建一张指定大小的纯色图片，支持圆角
+     *
+     * @param resources    Resources对象，用于创建BitmapDrawable
+     * @param width        图片的宽度
+     * @param height       图片的高度
+     * @param cornerRadius 图片的圆角，不需要则传0
+     * @param filledColor  图片的填充色
+     * @return 指定大小的纯色图片
+     */
+    public static BitmapDrawable createDrawableWithSize(Resources resources, int width, int height, int cornerRadius, @ColorInt int filledColor) {
+        Bitmap output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        if (filledColor == 0) {
+            filledColor = Color.TRANSPARENT;
+        }
+
+        if (cornerRadius > 0) {
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setStyle(Paint.Style.FILL);
+            paint.setColor(filledColor);
+            canvas.drawRoundRect(new RectF(0, 0, width, height), cornerRadius, cornerRadius, paint);
+        } else {
+            canvas.drawColor(filledColor);
+        }
+        return new BitmapDrawable(resources, output);
+    }
+
+    /**
+     * 设置Drawable的颜色
+     * <b>这里不对Drawable进行mutate()，会影响到所有用到这个Drawable的地方，如果要避免，请先自行mutate()</b>
+     */
+    public static ColorFilter setDrawableTintColor(Drawable drawable, @ColorInt int tintColor) {
+        LightingColorFilter colorFilter = new LightingColorFilter(Color.argb(255, 0, 0, 0), tintColor);
+        if(drawable != null){
+            drawable.setColorFilter(colorFilter);
+        }
+        return colorFilter;
+    }
+
+    /**
+     * 由一个drawable生成bitmap
+     */
+    public static Bitmap drawableToBitmap(Drawable drawable) {
+        if (drawable == null)
+            return null;
+        else if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        }
+
+        int intrinsicWidth = drawable.getIntrinsicWidth();
+        int intrinsicHeight = drawable.getIntrinsicHeight();
+
+        if (!(intrinsicWidth > 0 && intrinsicHeight > 0))
+            return null;
+
+        try {
+            Bitmap.Config config = drawable.getOpacity() != PixelFormat.OPAQUE ? Bitmap.Config.ARGB_8888
+                    : Bitmap.Config.RGB_565;
+            Bitmap bitmap = Bitmap.createBitmap(intrinsicWidth, intrinsicHeight, config);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 创建一张渐变图片，支持韵脚。
+     *
+     * @param startColor 渐变开始色
+     * @param endColor   渐变结束色
+     * @param radius     圆角大小
+     * @param centerX    渐变中心点 X 轴坐标
+     * @param centerY    渐变中心点 Y 轴坐标
+     * @return 返回所创建的渐变图片。
+     */
+    @TargetApi(16)
+    public static GradientDrawable createCircleGradientDrawable(@ColorInt int startColor,
+                                                                @ColorInt int endColor, int radius,
+                                                                @FloatRange(from = 0f, to = 1f) float centerX,
+                                                                @FloatRange(from = 0f, to = 1f) float centerY) {
+        GradientDrawable gradientDrawable = new GradientDrawable();
+        gradientDrawable.setColors(new int[]{
+                startColor,
+                endColor
+        });
+        gradientDrawable.setGradientType(GradientDrawable.RADIAL_GRADIENT);
+        gradientDrawable.setGradientRadius(radius);
+        gradientDrawable.setGradientCenter(centerX, centerY);
+        return gradientDrawable;
+    }
+
+
+    /**
+     * 动态创建带上分隔线或下分隔线的Drawable。
+     *
+     * @param separatorColor 分割线颜色。
+     * @param bgColor        Drawable 的背景色。
+     * @param top            true 则分割线为上分割线，false 则为下分割线。
+     * @return 返回所创建的 Drawable。
+     */
+    public static LayerDrawable createItemSeparatorBg(@ColorInt int separatorColor, @ColorInt int bgColor, int separatorHeight, boolean top) {
+
+        ShapeDrawable separator = new ShapeDrawable();
+        separator.getPaint().setStyle(Paint.Style.FILL);
+        separator.getPaint().setColor(separatorColor);
+
+        ShapeDrawable bg = new ShapeDrawable();
+        bg.getPaint().setStyle(Paint.Style.FILL);
+        bg.getPaint().setColor(bgColor);
+
+        Drawable[] layers = {separator, bg};
+        LayerDrawable layerDrawable = new LayerDrawable(layers);
+
+        layerDrawable.setLayerInset(1, 0, top ? separatorHeight : 0, 0, top ? 0 : separatorHeight);
+        return layerDrawable;
+    }
+
+
+    /////////////// VectorDrawable /////////////////////
+
+    public static
+    @Nullable
+    Drawable getVectorDrawable(Context context, @DrawableRes int resVector) {
+        try {
+            return AppCompatResources.getDrawable(context, resVector);
+        } catch (Exception e) {
+            Log.d("BitmapUtils", "Error in getVectorDrawable. resVector=" + resVector + ", resName=" + context.getResources().getResourceName(resVector) + e.getMessage());
+            return null;
+        }
+    }
+
+    public static Bitmap vectorDrawableToBitmap(Context context, @DrawableRes int resVector) {
+        Drawable drawable = getVectorDrawable(context, resVector);
+        if (drawable != null) {
+            Bitmap b = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(b);
+            drawable.setBounds(0, 0, c.getWidth(), c.getHeight());
+            drawable.draw(c);
+            return b;
+        }
+        return null;
+    }
 }
--- androidutils/src/main/java/com/github/androidtools/ToastUtils.java
+++ androidutils/src/main/java/com/github/androidtools/ToastUtils.java
@@ -8,24 +8,34 @@ import android.widget.Toast;
  */
 public class ToastUtils {
     private static Toast mToast = null;
+    private static Context mContext;
+    public static void init(Context context){
+        mContext=context;
+    }
     public static void showToast(Context context, String msg, int duration) {
         if (mToast == null) {
-            mToast = Toast.makeText(context, msg, duration);
+            mToast = Toast.makeText(context==null?mContext:context, msg, duration);
         } else {
             mToast.setText(msg);
             mToast.setDuration(duration);
         }
         mToast.show();
     }
+    public static void showToast(String msg, int duration) {
+        showToast(null,msg,duration);
+    }
 
     public static void showToast(Context context, String msg) {
         if (mToast == null) {
-            mToast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+            mToast = Toast.makeText(context==null?mContext:context, msg, Toast.LENGTH_SHORT);
         } else {
             mToast.setText(msg);
             mToast.setDuration(Toast.LENGTH_SHORT);
         }
         mToast.show();
+    }
+    public static void showToast(String msg) {
+        showToast(null,msg);
     }
     public static void cancelToast(){
         if(mToast!=null){
