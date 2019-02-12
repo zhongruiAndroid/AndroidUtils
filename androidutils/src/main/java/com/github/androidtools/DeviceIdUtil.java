2e18e9f2ecb21fede4f70ff2f6788adfeab72e97
Administrator <zr2013123@sina.com>
2019-02-12 10:20:01 +0800

    update file

--- androidutils/src/main/java/com/github/androidtools/DeviceIdUtil.java
+++ androidutils/src/main/java/com/github/androidtools/DeviceIdUtil.java
@@ -2,13 +2,26 @@ package com.github.androidtools;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresPermission;
 import android.telephony.TelephonyManager;
+import android.text.TextUtils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.NetworkInterface;
 import java.security.MessageDigest;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
+
+import static android.Manifest.permission.ACCESS_WIFI_STATE;
 
 /**
  * @author xc
@@ -92,7 +105,7 @@ public class DeviceIdUtil {
      * @param context 上下文
      * @return 设备的AndroidId
      */
-    private static String getAndroidId(Context context) {
+    public static String getAndroidId(Context context) {
         try {
             return Settings.Secure.getString(context.getContentResolver(),
                     Settings.Secure.ANDROID_ID);
@@ -174,5 +187,106 @@ public class DeviceIdUtil {
             sb.append(stmp);
         }
         return sb.toString().toUpperCase(Locale.CHINA);
+    }
+
+    /**
+     * Android  6.0 之前（不包括6.0）
+     * 必须的权限  <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+     * @param context
+     * @return
+     */
+    @RequiresPermission(ACCESS_WIFI_STATE)
+    private static String getMacDefault(Context context) {
+        String mac = "02:00:00:00:00:00";
+        if (context == null) {
+            return mac;
+        }
+
+        WifiManager wifi = (WifiManager) context.getApplicationContext()
+                .getSystemService(Context.WIFI_SERVICE);
+        if (wifi == null) {
+            return mac;
+        }
+        WifiInfo info = null;
+        try {
+            info = wifi.getConnectionInfo();
+        } catch (Exception e) {
+        }
+        if (info == null) {
+            return null;
+        }
+        mac = info.getMacAddress();
+        if (!TextUtils.isEmpty(mac)) {
+            mac = mac.toUpperCase(Locale.ENGLISH);
+        }
+        return mac;
+    }
+    /**
+     * Android 6.0（包括） - Android 7.0（不包括）
+     * @return
+     */
+    private static String getMacAddress() {
+        String WifiAddress = "02:00:00:00:00:00";
+        try {
+            WifiAddress = new BufferedReader(new FileReader(new File("/sys/class/net/wlan0/address"))).readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return WifiAddress;
+    }
+
+    /**
+     * 遍历循环所有的网络接口，找到接口是 wlan0
+     * 必须的权限 <uses-permission android:name="android.permission.INTERNET" />
+     * @return
+     */
+    private static String getMacFromHardware() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:", b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
+    }
+
+    /**
+     * 获取MAC地址
+     * @param context
+     * @return
+     */
+    @RequiresPermission(ACCESS_WIFI_STATE)
+    public static String getMacAddress(Context context) {
+        String mac = "02:00:00:00:00:00";
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            mac = getMacDefault(context);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mac = getMacAddress();
+        } else if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+            mac = getMacFromHardware();
+        }
+        return mac;
+    }
+
+    public static String getAndroidID(Context context) {
+        String id = Settings.Secure.getString(context.getContentResolver(),Settings.Secure.ANDROID_ID);
+        return id == null ? "" : id;
     }
 }
